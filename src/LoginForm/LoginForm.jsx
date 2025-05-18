303bb49da1217156223ce68176fafded8a49feef
Hlieb Kotin <gleb.kotiun@gmaill.com>
2025-05-18 19:16:24 +0200

    add form styles

--- src/LoginForm/LoginForm.jsx
+++ src/LoginForm/LoginForm.jsx
@@ -1,6 +1,7 @@
 import { Field, Form, Formik } from "formik";
 import { useContext } from "react";
 import { AuthContext } from "../providers/AuthProvider";
+import s from "./LoginForm.module.css";
 
 const LoginForm = () => {
   const { handleLogin } = useContext(AuthContext);
@@ -18,10 +19,22 @@ const LoginForm = () => {
   return (
     <div>
       <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-        <Form>
-          <Field name="username" type="text" placeholder="Enter name" />
-          <Field name="password" type="password" placeholder="Enter name" />
-          <button type="submit">Submit</button>
+        <Form className={s.form}>
+          <Field
+            className={s.input}
+            name="username"
+            type="text"
+            placeholder="Enter name"
+          />
+          <Field
+            className={s.input}
+            name="password"
+            type="password"
+            placeholder="Enter name"
+          />
+          <button className={s.btn} type="submit">
+            Submit
+          </button>
         </Form>
       </Formik>
     </div>
